732d981d4740777c656e4d1c8cce41295b3daf77
Clemen Dalgaard <clda@jppol.dk>
2025-11-03 11:44:39 +0100

    (no-issue) Add test for feature flag date

--- src/components/createFeatureFlag/test/featureFlag.test.ts
+++ src/components/createFeatureFlag/test/featureFlag.test.ts
@@ -4,7 +4,7 @@ import type {CreateFeatureFlagInput} from "@/types/featureFlag";
 
 describe("validateFeatureFlagInput", () => {
     it("returns error if name is less than 2 characters", () => {
-        const input = { id: 1, user_id: 1, name: "A", is_active: true, description: "En god beskrivelse", strategy: "NO_STRATEGY", start_time: "2025-11-01-12:00", end_time: "2025-11-10-15:00", created_at: "2025-10-10-15:00", updated_at: "", deleted_at: ""
+        const input = { user_id: 1, name: "A", is_active: true, description: "En god beskrivelse", strategy: "NO_STRATEGY", start_time: "2025-11-01-12:00", end_time: "2025-11-10-15:00", created_at: "2025-10-10-15:00", updated_at: "", deleted_at: ""
         } satisfies CreateFeatureFlagInput
         const actualOutput = validateFeatureFlagInput(input)
 
@@ -12,12 +12,18 @@ describe("validateFeatureFlagInput", () => {
     });
 
     it("returns error if description is less than 5 characters", () => {
-        const input = { id: 1, user_id: 1, name: "A very good name", is_active: true, description: "En", strategy: "NO_STRATEGY", start_time: "2025-11-01-12:00", end_time: "2025-11-10-15:00", created_at: "2025-10-10-15:00", updated_at: "", deleted_at: ""
+        const input = { user_id: 1, name: "A very good name", is_active: true, description: "En", strategy: "NO_STRATEGY", start_time: "2025-11-01-12:00", end_time: "2025-11-10-15:00", created_at: "2025-10-10-15:00", updated_at: "", deleted_at: ""
         } satisfies CreateFeatureFlagInput
         const actualOutput = validateFeatureFlagInput(input)
 
         expect(actualOutput).toBe("Feature flaggets beskrivelse skal være mindst 5 tegn")
     });
 
-    // More tests will follow
+    it("returns error if start time is after end time", () => {
+        const input = { user_id: 1, name: "A very good name", is_active: true, description: "A very good description", strategy: "NO_STRATEGY", start_time: "2025-12-01-12:00", end_time: "2025-11-10-15:00", created_at: "2025-10-10-15:00", updated_at: "", deleted_at: ""
+        } satisfies CreateFeatureFlagInput
+        const actualOutput = validateFeatureFlagInput(input)
+
+        expect(actualOutput).toBe("Sluttidspunkt skal være efter starttidspunkt og begge skal angives")
+    });
 })
