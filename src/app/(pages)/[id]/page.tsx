fc42da1d98d8cecf1ea8666fb87d84c502d1d27d
Clemen Dalgaard <clda@jppol.dk>
2025-11-03 10:52:33 +0100

    (no-issue) Move to new component

--- src/app/(pages)/[id]/page.tsx
+++ src/app/(pages)/[id]/page.tsx
@@ -1,15 +1,13 @@
 import CreateFeatureFlag from "@/components/createFeatureFlag/CreateFeatureFlag.component";
+import GetFeatureFlags from "@/components/getFeatureFlags/GetFeatureFlags.component";
 
 
 export default async function Page({params}: {params: {id: string}}) {
 
-    const response = await fetch(`http://localhost:3000/api/users/${params.id}`
-    );
-    const response2 = await fetch(`http://localhost:3000/api/featureFlags`);
+    const response = await fetch(`http://localhost:3000/api/users/${params.id}`);
 
     const user = await response.json();
 
-    const featureFlags = await response2.json();
 
     return (
         <div className="flex flex-col">
@@ -22,15 +20,7 @@ export default async function Page({params}: {params: {id: string}}) {
 
             <div className="flex flex-col mt-8 ml-5">
                 <h2 className="font-bold">Feature Flags:</h2>
-                <ul className="border-white border-2 rounded-md p-2 mt-1 max-w-lg">
-                    {featureFlags.map((flag: any) => (
-                        <li className="border-b-2 border-gray-500 p-1" key={flag.id}>
-                            <strong>{flag.name}</strong> - {flag.is_active ? "Active" : "Inactive"}
-                            {flag.description && `: ${flag.description}`}{" "}
-                            <em>(Created by {flag.user_name})</em>
-                        </li>
-                    ))}
-                </ul>
+                <GetFeatureFlags/>
             </div>
         </div>
     )
