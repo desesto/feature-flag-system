bae3a46644b7f4d4a756e051442256d0e76eaa6a
Clemen Dalgaard <clda@jppol.dk>
2025-11-06 12:48:21 +0100

    (no-issue) Create post request for fetching ff

--- src/app/api/featureFlags/[id]/route.ts
+++ src/app/api/featureFlags/[id]/route.ts
@@ -1,12 +1,11 @@
-import {NextRequest, NextResponse} from "next/server";
-import * as schema from "@/db/schema";
-import {featureFlagsTable} from "@/db/schema"
-import {eq} from "drizzle-orm/sql/expressions/conditions";
+import { and, eq, isNull } from "drizzle-orm";
 import {drizzle} from "drizzle-orm/node-postgres";
+import {featureFlagsTable} from "@/db/schema"
+import {type NextRequest, NextResponse} from "next/server";
 
 const db = drizzle(process.env.DATABASE_URL!);
 
-export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+export async function GET({ params }: { params: Promise<{ id: string }> }) {
     const { id } = await params;
     const featureFlagId = Number(id);
 
@@ -38,4 +37,26 @@ export async function DELETE(req: NextRequest) {
         .returning();
 
     return NextResponse.json(deletedFlag[0]);
+}
+
+export async function POST(req: NextRequest, { params }: { params: { id: string } }) {
+    const { id } = await params;
+    const flagName = decodeURIComponent(id).trim();
+    const apiKey = req.headers.get("x-api-key");
+
+    if (apiKey !== process.env.FEATURE_FLAG_API_KEY) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const featureFlag = await db
+        .select()
+        .from(featureFlagsTable)
+        .where(and(eq(featureFlagsTable.name, flagName), isNull(featureFlagsTable.deleted_at)));
+
+    if (featureFlag.length === 0) {
+        return NextResponse.json({ enabled: false });
+    }
+
+    const isEnabled = !!featureFlag[0].is_active;
+    return NextResponse.json({ enabled: isEnabled });
 }
